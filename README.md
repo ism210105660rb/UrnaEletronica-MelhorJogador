480e7112d128051d454fac7b95b8064adf22d02d
ism210105660rb <119175677+ism210105660rb@users.noreply.github.com>
2022-11-27 22:06:19 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,48 @@
+# Urna Eletronica Copa do Mundo
+
+Urna eletronica para escolha do melhor goleiro e melhor jogador da copa do mundo 2022
+
+# Sobre o Projeto
+
+Este projeto foi desenvolvido em HTML, CSS e Java Script.
+
+# Telas
+
+## Melhor Goleiro
+
+### Alisson - 38111
+
+<br />
+<img src="images/print-alisson.png" alt="Login" width="400"/>
+<br />
+<br />
+
+### Weverton - 77222
+
+<br />
+<img src="images/print-weverton.png" alt="Login" width="400"/>
+<br />
+<br />
+
+## Melhor Jogador
+
+### Neymar - 10
+
+<br />
+<img src="images/print-neymar.png" alt="Login" width="400"/>
+<br />
+<br />
+
+### Richarlison - 09
+
+<br />
+<img src="images/print-richarlison.png" alt="Login" width="400"/>
+<br />
+<br />
+
+## FIM
+
+<br />
+<img src="images/fim.png" alt="Login" width="400"/>
+<br />
+<br />
---
+++ etapas.js
@@ -0,0 +1,58 @@
+let etapas = [
+    {
+        titulo: 'MELHOR GOLEIRO DA COPA',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Alisson Becker',
+                partido: 'Brasil',
+                fotos: [
+                    {url: 'alisson.jpeg', legenda: 'Goleiro'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Weverton Pereira da Silva',
+                partido: 'Brasil',
+                fotos: [
+                    {url: 'weverton.jpeg', legenda: 'Goleiro'}
+                ]
+            },
+            
+        ]
+    },
+
+    {
+        titulo: 'MELHOR JOGADOR DA COPA',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '10',
+                nome: 'Neymar Junior',
+                partido: 'Brasil',
+                vice: 'Tite',
+                fotos: [
+                    {url: 'neymar.jpeg', legenda: 'Jogador'},
+                    {url: 'tite.jpg', legenda: 'Técnico', small: true}
+
+                ]
+            },
+            {
+                numero: '09',
+                nome: 'Richarlison de Andrade',
+                partido: 'Brasil',
+                vice: 'Tite',
+                fotos: [
+                    {url: 'richer.jpeg', legenda: 'Jogador'},
+                    {url: 'tite.jpg', legenda: 'Técnico', small: true}
+
+                ]
+            },
+            
+        ]
+    }
+    
+
+
+]
